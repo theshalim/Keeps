593395a3f832c130f803069163e02f861fc71c1d
Reza <shalim.reza@yahoo.com>
2024-05-16 00:46:53 +0600

    Add project files.

---
+++ Keeps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Keeps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
